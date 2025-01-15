5bda1340b54b55d46569124284489c256750a085
waflawe <popejpivaoleg@gmail.com>
2025-01-15 20:06:15 +0900

    Add spectacular schema and docs

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("vps.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="schema-docs"),
 ]
+
+admin.site.site_header = "Test Assignment"
